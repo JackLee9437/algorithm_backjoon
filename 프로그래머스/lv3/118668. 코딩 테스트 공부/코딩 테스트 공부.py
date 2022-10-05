8be202f46bdb9645c6888fc0a7e91d1ae80c7cf9
Jack <95907671+JackLee9437@users.noreply.github.com>
2022-10-05 14:47:04 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 16.46 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
+++ 프로그래머스/lv3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
@@ -3,6 +3,7 @@ from collections import deque
 def solution(alp, cop, problems):
     alpMax, copMax = max(problems, key=lambda x : x[0])[0], max(problems, key=lambda x : x[1])[1]
     problems.sort()
+    problems = [[0,0,1,0,1],[0,0,0,1,1]] + problems
     dp = [[int(1e9)] * 151 for _ in range(151)]
     dp[alp][cop] = 0
     
@@ -26,13 +27,6 @@ def solution(alp, cop, problems):
                 if dp[tmp_alp][tmp_cop] > tmp_time :
                     dp[tmp_alp][tmp_cop] = tmp_time
                     que.append((tmp_alp, tmp_cop, tmp_time))
-
-        if algoPower < 150 and dp[algoPower+1][codePower] > time+1 :
-            dp[algoPower+1][codePower] = time+1
-            que.append((algoPower+1, codePower, time+1))
-        if codePower < 150 and dp[algoPower][codePower+1] > time+1 :
-            dp[algoPower][codePower+1] = time+1
-            que.append((algoPower, codePower+1, time+1))
         
     
     for r in range(150, alpMax-1, -1) :
